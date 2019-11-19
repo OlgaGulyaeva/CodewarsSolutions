0abfc6bffbb156fe9d40b8374566883e3d7f97af
OG <goa.clf@gmail.com>
2019-11-19 15:35:03 -0800

    added a new kata 101 Dalmatians

--- KataCodewars.js
+++ KataCodewars.js
@@ -453,3 +453,19 @@ return sum;
 
 }
 ```
+```
+8 kyu
+101 Dalmatians - squash the bugs, not the dogs!
+JavaScript:
+function howManyDalmatians(number) {
+
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+  if (number <= 10) return dogs[0];
+  if (number > 10 && number <= 50) return dogs[1];
+  if (number > 50 && number <= 100) return dogs[2];
+  else return dogs[3];
+
+
+}
+```
